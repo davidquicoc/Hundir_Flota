119da425b30efce3051496acadb49702a66e4c66
davidquicoc <davidquicoc@gmail.com>
2025-12-08 15:28:01 +0100

    Actualización de contenido y código en los ficheros

--- script.js
+++ script.js
@@ -1,117 +1,121 @@
-//  Variables globales
-var filaTablero = 10;
-var celdaTablero = 10;
-var barcoTot = 10;
+const filaTablero = 10;
+const celdaTablero = 10;
 
-//  Arrays con datos para los barcos
-var barcTamañ = [1, 1, 1, 1, 2, 2, 3, 3, 4, 5];
-var barcOrient = ['h', 'v'];
-//  Array que almacena barcos
-var barcoPosic = [];
+const tamaño = [1, 2, 3, 4, 5];
+const orientacion = ['h', 'v'];
+var barcos = [];
 
-/*
-var n_intentos = 50;
-document.getElementById('intento').value = n_intentos;
-*/
-
-//  Tablero
 const tablero = document.getElementById('tablero');
 
-//  Checkbox de comprobación
-const input1 = document.getElementById('check1b');
-const input2 = document.getElementById('check2b');
-const input3 = document.getElementById('check3b');
-const input4 = document.getElementById('check4b');
-const input5 = document.getElementById('check5b');
+const inputs = {
+    1: document.getElementById('check1b'),
+    2: document.getElementById('check2b'),
+    3: document.getElementById('check3b'),
+    4: document.getElementById('check4b'),
+    5: document.getElementById('check5b')
+};
 
-//  Iniciar eventos de tablero y barcos
 document.addEventListener('DOMContentLoaded', function() {
     iniciarTablero();
-    //colocarBarcos();
-    inicializarEvento();
+    colocarBarcos();
+    eventoTocar();
 });
 
-//  Construye el #tablero con sus bloques
 function iniciarTablero() {
-    //  Variable que guarda la estructura HTML de dentro del #tablero
     let html = "";
 
     let letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
 
-    //  Insertar fila de números
     for (let i = 0; i < filaTablero; i++) {
         html += "<div class='casilla casilla-simb' style='grid-column:1/2; grid-row:" + (i + 1)+ "/" + (i+2) + ";'>" + (i+1) + "</div>";
     }
 
-    //  Añadir casilla vacía
     html += "<div class='casilla casilla-vacia' style='grid-column:1/2; grid-row:11/12;'></div>";
 
-    //  Insertar columnas de letras
     for (let i = 0; i < letras.length; i++) {
         html += "<div class='casilla casilla-simb' style='grid-row: 11/12; grid-column:" + (i + 2) + "/" + (i + 3) + ";'>" + letras[i] + "</div>";
     }
 
-    //  Insertar casillas restantes 
     for (let i = 0; i < filaTablero; i++) {
         for (let j = 0;j < celdaTablero; j++) {
             html += "<div id='cell-" + (i+1) + "-" + (j+1) + "' class='casilla casilla-hueco' style='grid-column:" + (j + 2) + "/" + (j+3) + "; grid-row:" + (i + 1) + "/" + (i + 2) + ";'>&nbsp;</div>";
         }
     }
-    //  Añadir html en #tablero
     document.getElementById('tablero').innerHTML = html;
 }
 
-/*
-
-EN DESARROLLO
-
 function colocarBarcos() {
-    for (let i = 0; i < barcoTot; i++) {
-        let tamaño = barcTamañ[i];
+    for (let i = 0; i < tamaño.length; i++) {
+        let tam = tamaño[i];
         let colocado = false;
 
         while (!colocado) {
-            let orient = barcOrient[Math.floor(Math.random() * barcOrient.length)];
-            let fila = Math.floor(Math.random() * filaTablero) + 1;
-            let celd = Math.floor(Math.random() * celdaTablero) + 1;
+            let orient = orientacion[Math.floor(Math.random() * orientacion.length)];
 
-            let posiciones = [];
-            for (let j = 0; j < tamaño; j++) {
-                let f = fila;
-                let c = celd;
+            let filaMax = orient == 'v' ? filaTablero - tam + 1 : filaTablero;
+            let celdaMax = orient == 'h' ? celdaTablero - tam + 1 : celdaTablero;
+
+            let filaIni = Math.floor(Math.random() * filaMax) + 1;
+            let celdaIni = Math.floor(Math.random() * celdaMax) + 1;
+
+            let celdasData = [];
+            let valido = true;
+
+            for (let j = 0; j < tam; j++) {
+                let f = filaIni;
+                let c = celdaIni;
+
                 if (orient == 'h') {
-                    c += j;
+                    c = celdaIni + j;
                 } else {
-                    f += j;
+                    f = filaIni + j;
                 }
-                
+
+                let celdaDOM = getCell(f, c);
+                if (!celdaDOM || celdaDOM.classList.contains('barco')) {
+                    valido = false;
+                    break;
+                }
+                celdasData.push(celdaDOM.id);
+            }
+
+            if (valido) {
+                for (let k = 0; k < celdasData.length; k++) {
+                    let celdaDOM = document.getElementById(celdasData[k]);
+                    celdaDOM.classList.add('barco'); // marcar como barco (oculto)
+                }
+
+                barcos.push({
+                    tamaño: tam,
+                    orientacion: orient,
+                    celdas: celdasData,
+                    tocadas: []
+                });
+
+                colocado = true;
             }
         }
     }
 }
-*/
 
-function inicializarEvento() {
-    //  Variable que obtiene todas las casillas
-    let casillas = document.querySelectorAll(".casilla-hueco");
-    //  Bucle que recorre cada casilla hasta a veriguar cual hizo la función de click
-    for (let i = 0; i < casillas.length; i++) {
-        let casilla = casillas[i];
+function eventoTocar() {
+    const casillasEvt = document.querySelectorAll('.casilla-hueco');
+    for (let i = 0; i < casillasEvt.length; i++) {
+        let casilla = casillasEvt[i];
         casilla.addEventListener('click', function() {
-            //  Cambiar fondo de color de la casilla
-            casilla.style.background = "#30466e";
+            if (casilla.classList.contains('barco')) {
+                casilla.style.background = 'red';
+            } else {
+                casilla.style.background = 'gray';
+            }
         });
     }
 }
 
-function comprobarBarco() {
-    /*if ()*/
+function getCell(fila, celda) {
+    return document.getElementById("cell-" + fila + "-" + celda);
 }
 
-//  Botón #restart hará la función restartGame() cuando se clickee en él
-document.getElementById('restart').addEventListener('click', restartGame);
-
-//  Refirige a index.html
-function restartGame() {
-    window.location.href = "index.html";
-}
+document.getElementById('restart').addEventListener('click', function() {
+    window.location.href = './index.html';
+});
